35f9763e6e0585c8e503b81c28721ea393462978
agsjorda <agsjorda@gmail.com>
2022-03-12 23:08:42 +0800

    uploaded react smooth scroll website

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,60 @@
+import svg1 from '../../images/svg-1.svg';
+import svg2 from '../../images/svg-2.svg';
+import svg3 from '../../images/svg-3.svg';
+import svg4 from '../../images/svg-4.svg';
+import svg5 from '../../images/svg-5.svg';
+import svg6 from '../../images/svg-6.svg';
+
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Bank',
+  headLine: 'Unlimited Transactions with zero fees',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Get Started',
+  imgStart: true,
+  img: svg1,
+  alt: 'forest',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Unlimited Access',
+  headLine: 'Unlimited Transactions with zero fees',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Just Do It',
+  imgStart: true,
+  img: svg2,
+  alt: 'hacker',
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Join The Movement',
+  headLine: 'Sign Up for your favourite apps',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Sign up',
+  imgStart: true,
+  img: svg3,
+  alt: 'mobile',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
